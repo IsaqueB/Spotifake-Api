64bba411ed598c0a913c6be356663aced2006525
IsaqueB <isaquebz.dev@gmail.com>
2022-04-26 17:14:25 -0300

    use default variable db connect

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const mongo_url = process.env.MONGO_URL  || "mongodb://localhost:27017/spotifake-api"
 
-mongoose.connect(process.env.MONGO_URL,{
+mongoose.connect(mongo_url,{
 })
 
 mongoose.connection.on("connected", _ => {
